e91e83de61e1b8eb7bf89df1ccb40d40fca98bca
A7MeD555 <43023294+A7MeD555@users.noreply.github.com>
2018-11-13 22:37:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,10 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+client.on("ready", () => {
+  console.log("Vulnix | Logged in! Server count: ${client.guilds.size}");
+  client.user.setGame(`-bc`);
+});
 
 
 
@@ -54,7 +58,27 @@ client.on('ready', () => {
     }
     });
 
-
+ client.on('message', message => {
+    if (message.content.startsWith(prefix + "bot")) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setAuthor(client.user.username,client.user.avatarURL)
+            .setThumbnail(client.user.avatarURL)
+            .setColor('RANDOM')
+            .setTitle('``INFO ZeusSHOP Community ©`` ')
+            .addField('``My Ping``' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('``RAM Usage``', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
+            .addField('``servers``', [client.guilds.size], true)
+            .addField('``channels``' , `[ ${client.channels.size} ]` , true)
+            .addField('``Users``' ,`[ ${client.users.size} ]` , true)
+            .addField('``My Name``' , `[ ${client.user.tag} ]` , true)
+            .addField('``My ID``' , `[ ${client.user.id} ]` , true)
+			      .addField('``My Prefix``' , `[ ! ]` , true)
+			      .addField('``My Language``' , `[ Java Script ]` , true)
+			      .setFooter('By | ↄᓄב I♛#3933')
+    })
+}
+});
 
 
 
